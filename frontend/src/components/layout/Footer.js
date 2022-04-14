0802b36bd920e7ccde2afb92416e796272d135a1
Anurag15v <anuragvaibhav7@gmail.com>
2022-04-14 19:21:36 +0530

    changed text

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -45,7 +45,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by <a href="https://cruip.com">Cruip</a>. All right reserved</div>
+            <div className="footer-copyright">Made by <a href="https://github.com/Anurag15v/Sign_Language_SIH_Website">TeamForBetterIndia</a> with ❤️.</div>
           </div>
         </div>
       </div>
--- frontend/src/components/sections/Hero.js
+++ frontend/src/components/sections/Hero.js
@@ -57,11 +57,11 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Powerfull Sign Language Detection Software<span className="text-color-primary"> Shakti</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Sign Language Detection software with Sign to Text and Speech to Sign Modes.Let's unite to support our specially abled friends.
               </p>
               <Functionality />
             </div>
